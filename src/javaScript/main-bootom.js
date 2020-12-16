d769e0467b8d6fb09d6bd46bfe1a79f23d4ef60b
Damian Ligmann <ligmann.damian@gmail.com>
2020-12-16 18:41:11 +0100

    complitetd components no rwd or navigation complitet 1/2

---
+++ src/javaScript/main-bootom.js
@@ -0,0 +1,15 @@
+/*************
+ **	SWIPER	**
+ **************/
+
+//PARTNERS
+new Swiper('.swiper-container.js-partners', {
+	spaceBetween: 30,
+	centeredSlides: true,
+	autoplay: {
+		delay: 5500,
+		disableOnInteraction: false,
+	},
+});
+
+//INFORMATIONS TWO COLUMNS
--- src/javaScript/main.js
+++ src/javaScript/main.js
@@ -1,1 +1,1 @@
-console.log('hello word')
+let colapse = document.getElementById("myDIV");
